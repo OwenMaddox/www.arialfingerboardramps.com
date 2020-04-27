3a0909653fdd035618f7d4724605a35a7bbe75b9
OwenMaddox <Owen@maddoxbox.com>
2020-04-27 17:29:50 -0600

    Fix overlap on xl devices for kicker and kicker with rail pages

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    height: {
+      sm: "40px",
+      md: "60px",
+      lg: "80px",
+      xl: "100px"
+    }
+  }
+};
+
+module.exports = {
+  theme: {
+    spacing: {
+      sm: "10px",
+      md: "15px",
+      lg: "15px",
+      xl: "20px"
+    }
+  }
+};
